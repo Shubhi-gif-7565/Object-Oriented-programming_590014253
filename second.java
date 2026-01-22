d415fc256ac3bbc86d4995ca53fa98fd7d94b19b
Shubhi-gif-7565 <palbhumi334@gmail.com>
2026-01-22 11:16:37 +0530

    Add files via upload

---
+++ second.java
@@ -0,0 +1,8 @@
+class second {
+    public static void main(String[] args) {
+        System.out.println("Hello world with me");
+        System.out.println("Hello world with me");
+        System.out.println("Hello world with me");
+        
+    }
+}
