235de386111f028fb495f743ddfd232d537a3e5d
Shubhi-gif-7565 <palbhumi334@gmail.com>
2026-01-31 11:41:45 +0530

    Add files via upload

---
+++ withouts.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class withouts{
+    int n ;
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        withouts m = new withouts();
+        m.n = sc.nextInt();
+        System.out.println(m.n); 
+
+    }
+}
---
+++ withs.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class withs{
+    //with static
+    static int n ;
+    public static void main(String[] args) {
+        
+    
+    Scanner sc = new Scanner(System.in);
+    n = sc.nextInt();
+    System.out.print(n);
+    }
+
+}
+
